5576d34d0f96767f0761696308e44f809ce37741
mukuashi <572512250@qq.com>
2018-05-25 02:40:58 +0800

    add system img data

--- src/routes/Home/components/Content4.js
+++ src/routes/Home/components/Content4.js
@@ -2,6 +2,8 @@ import React, { PureComponent } from 'react';
 import TweenOne, { TweenOneGroup } from 'rc-tween-one';
 import OverPack from 'rc-scroll-anim/lib/ScrollOverPack';
 import Gallery from 'react-grid-gallery';
+import { Slider, Icon } from 'antd';
+
 import { data } from 'config/system';
 
 const { text, block } = data.content4;
@@ -10,9 +12,30 @@ export default class Content extends PureComponent {
   static defaultProps = {
     className: 'content4',
   };
+  state = {
+    boxValue: 20,
+  }
+  handleChange = (value) => {
+    this.setState({
+      boxValue: value,
+    });
+  }
 
   render() {
     const props = { ...this.props };
+
+    const { boxValue } = this.state;
+    const marks = {
+      0: '最小边距',
+      20: '相册边距推荐值',
+      50: {
+        style: {
+          color: '#f50',
+        },
+        label: <strong>最大边距</strong>,
+      },
+    };
+
     return (
       <section {...props} className="content-template-wrapper content4-wrapper">
         <OverPack location={props.id} className={`content-template ${props.className}`}>
@@ -39,8 +62,23 @@ export default class Content extends PureComponent {
               ))
             }
           </TweenOne>
-          <TweenOneGroup
+          <TweenOne
             key="c"
+            animation={{ x: '+=30', opacity: 0, type: 'from', ease: 'easeOutQuad' }}
+            className={`${props.className}-slider`}
+            reverseDelay={300}
+          >
+            <Slider
+              // vertical 垂直布局
+              min={0}
+              max={50}
+              marks={marks}
+              defaultValue={20}
+              onChange={this.handleChange}
+            />
+          </TweenOne>
+          <TweenOneGroup
+            key="d"
             className={`${props.className}-img-wrapper`}
             enter={[
               { opacity: 0, duration: 0, type: 'from', delay: 400 },
@@ -49,7 +87,8 @@ export default class Content extends PureComponent {
             leave={{ y: '+=30', opacity: 0, ease: 'easeOutQuad' }}
           >
             <Gallery
-              margin={6}
+              margin={boxValue}
+              rowHeight={200 + boxValue}                // 图片当前列的默认高度
               images={block}
               showLightboxThumbnails         // 弹层后显示底部预览幻灯片缩略图
               backdropClosesModal            // 弹层后点击其他地方支持遮罩关闭
